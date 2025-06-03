17ef111a10ba2b2df5b69e6f057fd8945a1bcbab
DarwinEV <90359015+DarwinEV@users.noreply.github.com>
2025-06-03 17:03:47 -0400

    Bid Discovery Agent Fix
    We were able to set up a bid finder agent and web search sub agent using Google ADK and selenium. We are looking to perform further prompt engineering to fix issues like opening the chrome instance before the user guidance etc. Next step is to integrate with the RAG agent and download the bid documents for the next agent in the workflow.

--- backend/agents/Bid_Discovery/agent.py
+++ backend/agents/Bid_Discovery/agent.py
@@ -20,16 +20,18 @@ FEATURES:
 
 from google.adk.agents.llm_agent import Agent
 from .shared_libraries import constants
-from .sub_agents import bid_search_agent
+from .sub_agents.search_results.agent import search_results_agent
 from . import prompt
 
 root_agent = Agent(
     name=constants.AGENT_NAME,
     description=constants.DESCRIPTION,
     model=constants.MODEL,
-    # tools=[]  # check the github sample code for the tool.
+    # tools=[
+    #     <specify_tool>,
+    # ],  
     instruction=prompt.ROOT_PROMPT,
     sub_agents=[
-        bid_search_agent
+        search_results_agent
     ],
 )
---
+++ backend/agents/Bid_Discovery/tools/__init__.py
@@ -0,0 +1,12 @@
+"""
+RAG Tools package for interacting with Vertex AI RAG corpora.
+"""
+## shows how we init the tools ->
+# from .big_query_connector import big_query_connector 
+# from .utils import (
+#     function,
+# )
+
+# __all__ = [
+#     "big_query_connector",
+# ]
